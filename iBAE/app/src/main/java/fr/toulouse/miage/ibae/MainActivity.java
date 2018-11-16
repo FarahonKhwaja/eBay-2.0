bc2f6b02cb10a4d9223f0cdf5baf61299b60aef6
Unknown <pierre.liaubet2@gmail.com>
2018-11-16 17:47:00 +0100

    Fragment plus propres qui marchent

--- iBAE/app/src/main/java/fr/toulouse/miage/ibae/MainActivity.java
+++ iBAE/app/src/main/java/fr/toulouse/miage/ibae/MainActivity.java
@@ -3,13 +3,19 @@ package fr.toulouse.miage.ibae;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import fr.toulouse.miage.ibae.Fragments.HomeFragment;
+import fr.toulouse.miage.ibae.Fragments.VendreFragment;
 
-    private TextView mTextMessage;
+public class MainActivity extends FragmentActivity {
+
+    private Fragment contenu;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //TODO : Fragment home
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //TODO : Fragment profil
                     return true;
             }
             return false;
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
