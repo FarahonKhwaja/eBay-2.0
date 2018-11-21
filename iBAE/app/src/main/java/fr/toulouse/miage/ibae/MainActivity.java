e63bf5656c1f43ccc0e35a28bd9bd0ab6a7ccd24
Pierre <pierre.liaubet2@gmail.com>
2018-11-21 09:11:37 +0100

    Gros débile de Pierre répare tes conneries

--- iBAE/app/src/main/java/fr/toulouse/miage/ibae/MainActivity.java
+++ iBAE/app/src/main/java/fr/toulouse/miage/ibae/MainActivity.java
@@ -2,20 +2,17 @@ package fr.toulouse.miage.ibae;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import fr.toulouse.miage.ibae.Fragments.HomeFragment;
 import fr.toulouse.miage.ibae.Fragments.VendreFragment;
 
 public class MainActivity extends FragmentActivity {
 
-    private Fragment contenu;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,13 +21,15 @@ public class MainActivity extends FragmentActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    //TODO : Fragment home
+                    replaceFragment(R.id.contenu, new HomeFragment(), "home");
                     return true;
                 case R.id.navigation_dashboard:
+                    replaceFragment(R.id.contenu, new VendreFragment(), "vendre");
                     return true;
                 case R.id.navigation_notifications:
                     //TODO : Fragment profil
                     return true;
+
             }
             return false;
         }
@@ -40,10 +39,25 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        addFragment(R.id.contenu, new HomeFragment(), "home");
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+    }
+
+    protected void addFragment(int containerViewId, Fragment fragment, String fragmentTag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(containerViewId, fragment, fragmentTag)
+                .commit();
+    }
+
+    protected void replaceFragment(int containerViewId, Fragment fragment, String fragmentTag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerViewId, fragment, fragmentTag)
+                .commit();
     }
 
 }
